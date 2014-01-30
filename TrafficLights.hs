27147f3a9c9dc3acaf7480a6a96e6c8105a63165
Willian Junior <guns945@gmail.com>
2014-01-30 10:01:51 +0000

    muxn implemented and tested. trafficlights now fully extensible to n bits. still need to do reset

--- TrafficLights.hs
+++ TrafficLights.hs
@@ -12,9 +12,13 @@ import HDL.Hydra.Circuits.StandardCircuits
 -- trafficLight version 1
 
 trafficLights1 :: Clocked a => a -> (a,a,a)
-trafficLights1 reset = (out,cout,sum)
+trafficLights1 reset = (g,a,r)
   where
-    (out,cout,sum) = counter3 zero
+    counter = countern 4 one rst
+    g = muxn 4 counter [one,  one,  one,  zero, zero, zero, zero, zero, zero, zero, zero, zero, zero, zero, zero, zero]
+    a = muxn 4 counter [zero, zero, zero, one,  zero, zero, zero, zero, one,  zero, zero, zero, zero, zero, zero, zero]
+    r = muxn 4 counter [zero, zero, zero, zero, one,  one,  one,  one,  zero, zero, zero, zero, zero, zero, zero, zero]
+    rst = or2 reset (andw counter)
 
 ------------------------------------------------------------------------
 -- 1-bit counter block
@@ -34,3 +38,27 @@ counter3 reset = (x0,x1,x2)
     (x0,c0) = counter1 one reset
     (x1,c1) = counter1 c0 reset
     (x2,c2) = counter1 c1 reset
+
+------------------------------------------------------------------------
+-- n-bit counter block generator
+
+countern :: Clocked a => Int -> a -> a -> [a]
+countern 0 _ _ = []
+countern n cin reset = (countern (n-1) cout reset) ++ [x]
+  where
+    (x,cout) = counter1 cin reset
+
+temp :: [a] -> (a,a,a)
+temp (x:(y:(z:xs))) = (x,y,z)
+
+------------------------------------------------------------------------
+-- n-bit mux block generator
+
+muxn :: Clocked a => Int -> [a] -> [a] -> a
+muxn 1 (sel:[]) (x:(y:[])) = mux1 sel x y
+muxn n (sel:sels) input = mux1 sel x y
+  where
+    x = muxn (n-1) sels inputlo
+    y = muxn (n-1) sels inputho
+    (inputlo,inputho) = splitAt ((length input) `div`2) input
+
